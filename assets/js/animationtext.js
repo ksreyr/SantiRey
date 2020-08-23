1c97400d3e230061c7825fc00aa291435b48de32
Kevin Santiago Rey Rodriguez <kev_rey@uni-bremen.de>
2020-08-23 05:33:10 +0200

    new page

---
+++ assets/js/animationtext.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".typed", {
+	strings: [" Im Software Developer", " Im Sound Engineer", " Im Photographer"],
+	typeSpeed: 70,
+	backSpeed: 90,
+	loop: true,
+	smartBackspace: true,
+});
